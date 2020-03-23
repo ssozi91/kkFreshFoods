13103ec5e704dc2f38962f478f73da1a860198da
Frank Ssozi <frankssozi@franks-mbp.home>
2020-03-23 04:38:22 +0000

    uploaded all the initial files and folder.

---
+++ app/src/main/java/com/example/kkfreshfoods/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.kkfreshfoods.Prevalent;
+
+        import com.example.kkfreshfoods.Model.Users;
+
+public class Prevalent<Public> {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
